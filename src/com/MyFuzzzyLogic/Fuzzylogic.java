636f6c5491f4d18012e3ee1033ec86d18685b09e
Alnaser <alnaser.sidawi@gmail.com>
2016-02-15 00:31:25 +0100

    alnaser commit

--- src/com/MyFuzzzyLogic/Fuzzylogic.java
+++ src/com/MyFuzzzyLogic/Fuzzylogic.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fuzzylogic;
+package com.MyFuzzzyLogic;
 
 import java.io.File;
 import java.io.FileReader;
